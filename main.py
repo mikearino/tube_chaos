2bdd75659c503ca53658e9cbfe3d8e609b5a3a38
Mike <mikeylofgren@gmail.com>
2025-12-07 17:03:54 -0800

    basic controls worked in

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pygame
 
+from pygame.locals import(
+    K_RSHIFT,
+    K_LSHIFT
+)
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
     #check events
@@ -15,11 +23,20 @@ while running:
 
     screen.fill("purple")
 
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RSHIFT]:
+        player_pos.x += 300 * dt
+    if keys[pygame.K_LSHIFT]:
+        player_pos.x -= 300 * dt
+
+
     #Rendering:
 
     #flip() puts work on screen
     pygame.display.flip()
 
-clock.tick(60) #limits FPS to 60
+    dt = clock.tick(60) / 1000 #limits FPS to 60
 
 pygame.quit()
